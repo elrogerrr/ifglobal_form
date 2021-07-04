d994a83a3005b6f5603c159598825de995618352
hi <hi@ElRogeRRR-Mac-2.local>
2021-07-04 12:54:10 -0500

    agregado tokio

---
+++ app.py
@@ -0,0 +1,16 @@
+from re import DEBUG
+from flask import Flask, redirect, url_for, render_template, flash, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
