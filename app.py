dd7ec38f8dfc6f22ce04d3a548291c3ea69e1359
elrogerrr <elrogerrr@hotmail.com>
2021-07-13 17:34:41 -0500

    validaciones y errores

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, redirect, url_for, render_template, flash, request
 import forms
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.secret_key = 'm1y2s3e4c5r6e7t8k9e0y'
+csrf = CSRFProtect(app)
 
 @app.route('/')
 def index():
@@ -12,16 +15,23 @@ def index():
 def hello(name=None):
     return render_template('index.html', name=name)
 
-@app.route('/registro', methods=[ 'GET', 'POST' ] )
+@app.route('/registro',methods=['GET','POST'])
 def registro():
     titulo = 'Registro'
     registro_form  = forms.RegistroForm(request.form)
-    if request.method  == 'POST':
+    if request.method  == 'POST' and registro_form.validate():
         print (registro_form.username.data)
         print (registro_form.email.data)
         print (registro_form.password.data)
-
     return render_template('registro.html', titulo = titulo, form = registro_form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+@app.route('/info', methods=['GET', 'POST'])
+def info():
+    pass
 
 
 
--- forms.py
+++ forms.py
@@ -1,7 +1,9 @@
-from wtforms import Form, StringField, PasswordField
+from wtforms import Form, StringField, PasswordField, TextField, TextAreaField
 from wtforms.fields.html5 import EmailField
+from wtforms import validators
+
 
 class RegistroForm(Form):
-    username=StringField('Nombre')
+    username=StringField('Nombre',[validators.Length(min=4,max=10, message='Ingrese nombre valido'), validators.Required()])
     email=EmailField('Correo Electronico')
     password=PasswordField('Contraseña')
