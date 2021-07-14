1ce375c65cd9393db896f4cb5a5ac2d49c18cb5e
elrogerrr <elrogerrr@hotmail.com>
2021-07-14 00:23:58 -0500

    pass_cambio

--- forms.py
+++ forms.py
@@ -4,6 +4,14 @@ from wtforms import validators
 
 
 class RegistroForm(Form):
-    username=StringField('Nombre',[validators.Length(min=4,max=10, message='Ingrese nombre valido'), validators.Required()])
-    email=EmailField('Correo Electronico')
-    password=PasswordField('Contraseña')
+    username=StringField('Nombre',[
+        validators.Length(min=4,max=25, message='Ingrese nombre valido'),
+        validators.Required()
+        ])
+    email=EmailField('Correo Electronico',[
+        validators.Required()
+        ])
+    password=PasswordField('Contraseña',[
+        validators.Length(min=8,max=25, message='Ingrese contraseña valida'),
+        validators.Required()
+        ])
