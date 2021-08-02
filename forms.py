b3a05fa8a1c439d5fbfddcfe11278d178f856a3b
elrogerrr <elrogerrr@hotmail.com>
2021-08-02 16:33:12 -0500

    override_usuarios repetidos

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from wtforms import Form, StringField, PasswordField, TextField, TextAreaField, HiddenField, BooleanField
 from wtforms.fields.html5 import EmailField
 from wtforms import validators
+from models import User
 
 def length_honeypot(form, field):
     if len(field.data)>0:
@@ -21,6 +22,11 @@ class RegistroForm(Form):
         ])
     acepto=BooleanField('',[validators.Required()])
     honeypot = HiddenField('',[length_honeypot])
+    def validate_username(form,field):
+        username = field.data
+        user = User.query.filter_by(username=username).first()
+        if user is not None:
+            raise validators.ValidationError('El username ya se encuentra registrado, favor de considerar otra opción.!!!')
 
 class LoginForm(Form):
     email=EmailField('Correo Electronico',[
